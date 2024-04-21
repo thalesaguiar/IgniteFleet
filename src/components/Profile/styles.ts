3a78625e9750088f0511f887d20c7fa6d0cc571a
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-04-20 23:44:27 -0300

    feat: create new component

---
+++ src/components/Profile/styles.ts
@@ -0,0 +1,9 @@
+import {View} from 'react-native';
+import styled from 'styled-components';
+import theme from '../../theme';
+
+const Container = styled(View)`
+  flex: 1;
+  width: 100%;
+  background-color: ${theme.COLORS.GRAY_300};
+`;
